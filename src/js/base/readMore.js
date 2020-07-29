f0c9e8fe9d4012095282373920f2082e20980f86
dron5 <dron5@mail.ru>
2020-07-29 17:48:14 +0700

    Bugs deleting6

--- src/js/base/readMore.js
+++ src/js/base/readMore.js
@@ -1,18 +1,35 @@
 
+let readMoreButton = document.querySelector('.main__read-more');
+
+readMoreButton.addEventListener('click', () => {
+    let block = document.querySelector('.main__description');
+    block.classList.toggle('main__description--opend');
+    readMoreButton.classList.toggle('logo__show--more');
+    readMoreButton.classList.toggle('logo__show--less');
+    if (readMoreButton.classList.contains('logo__show--more')){
+        readMoreButton.textContent = 'Показать всё';
+    }else{
+        readMoreButton.textContent = 'Скрыть';
+    }
+});
+
+/*******************************************************/
+
 const buttons = document.querySelectorAll('.logo__show');
-const readMoreButton = document.querySelector('.main__read-more');
 
 for (let button of buttons) {
     button.addEventListener('click', () => {
         let name ='.' + String(button.parentNode.firstElementChild.classList[0]);
         let block = document.querySelector(name);
         block.classList.toggle('wrapper--opend');
+        
+        button.classList.toggle('logo__show--more');
+        button.classList.toggle('logo__show--less');
+        if (button.classList.contains('logo__show--more')){
+            button.textContent = 'Показать всё';
+        }else{
+            button.textContent = 'Скрыть';
+        }
+        
     })
 }
-/*******************************************************/
-
-
-readMoreButton.addEventListener('click', () => {
-    let block = document.querySelector('.main__description');
-    block.classList.toggle('main__description--opend');
-});
