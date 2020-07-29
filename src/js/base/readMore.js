c1c1a330dc1bbee5325956f0dbbf9680a3dd2722
dron5 <dron5@mail.ru>
2020-07-29 14:52:31 +0700

    Bugs deleting4

--- src/js/base/readMore.js
+++ src/js/base/readMore.js
@@ -1,7 +1,12 @@
-let readButton = document.querySelector('.logo__show');
 
-function showMe () {
-    let block = document.querySelector('.swiper-wrapper');
-    block.classList.toggle('swiper-wrapper--opend');
+const buttons = document.querySelectorAll('.logo__show');
+
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        let name ='.' + String(button.parentNode.firstElementChild.classList[0]);
+        let block = document.querySelector(name);
+        block.classList.toggle('wrapper--opend');
+    })
 }
-readButton.addEventListener('click', showMe);
+/*******************************************************/
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
 import '../scss/style.scss';
 
 import  './base/readMore.js';
-//import  './base/mobileMenu.js';
 import  './base/swiper_script.js';
 import  './base/burgerAction.js';
-//import  './base/modalWinAction.js';
