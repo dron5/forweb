63f8e63b8d510a92bb98e5753b9a90bb3ffcd279
dron5 <dron5@mail.ru>
2020-07-29 11:39:58 +0700

    Bugs deleting3

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../scss/style.scss';
 
 import  './base/readMore.js';
-import  './base/mobileMenu.js';
+//import  './base/mobileMenu.js';
 import  './base/swiper_script.js';
 import  './base/burgerAction.js';
-import  './base/modalWinAction.js';
+//import  './base/modalWinAction.js';
